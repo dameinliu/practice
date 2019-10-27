65ef4895b3b91232e0d13e6690435ab47a270eda
damein <dameinliu@foxmail.com>
2019-10-27 22:31:00 +0800

    20191027

--- scrapyprac/prac001.py
+++ scrapyprac/prac001.py
@@ -1,9 +1,42 @@
 import requests
+from bs4 import BeautifulSoup
 
-content = requests.get('https://localprod.pandateacher.com/python-manuscript/crawler-html/spider-men5.0.html').content.decode('utf8')
+res = requests.get('http://www.xiachufang.com/explore/').text
+bs = BeautifulSoup(res, 'html.parser')
 
-print(content)
+# food_lists = bs.find_all('div', class_='info pure-u')
+# foods = []
 
-f = open('content.html', 'a+', encoding='utf8')
-f.write(content.replace(u'\xa9',u''))
-f.close()
+# for food in food_lists:
+# 	food_name = food.find('a').text.strip()
+# 	food_url = "http://www.xiachufang.com"+food.find('a')['href']
+# 	food_ing = food.find('p', class_='ing ellipsis').text.strip()
+
+# 	foods.append([food_name, food_url, food_ing])
+
+# for food in foods:
+# 	print(food[0])
+
+	# print("菜谱名："+food_name+"\n"+"URL："+food_url+"\n"+"食材："+food_ing)
+	# print("====================")
+
+'''
+第二种思路
+'''
+
+names = bs.find_all('p', class_='name')
+urls = bs.find_all('p', class_='name')
+ings = bs.find_all('p', class_='ing ellipsis')
+
+foods = []
+
+for i in range(len(names)):
+	name = names[i].text.strip()
+	url = 'http://www.xiachufang.com'+urls[i].find('a')['href']
+	ing = ings[i].text.strip()
+
+	foods.append([name, url, ing])
+
+for food in foods:
+	print(food[0]+'\n'+food[1]+'\n'+food[2])
+	print('====================')
