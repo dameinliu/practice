78481769f7563c071fe3f9ef2d601b0656147fa7
damein <dameinliu@foxmail.com>
2019-12-05 22:38:55 +0800

    20191205

--- lyricscraper/lyricscraper.py
+++ lyricscraper/lyricscraper.py
@@ -2,3 +2,6 @@ import requests
 
 response = requests.get('https://music.163.com/#/artist/album?id=12046230')
 print(response.status_code)
+print("==========")
+print(response.text)
+print(response.content.decode())
